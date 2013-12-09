c75cbb7c3298d19316cbe165c6a30fc8cd43acc9
Pascal Guilcher <guilcher.p@sfeir.com>
2013-12-09 14:29:47 +0100

    update yeoman and generator + yo angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('pguKonamiApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
