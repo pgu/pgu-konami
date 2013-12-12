d0f0c9159c5a55a419ba19297699788a6668282d
Pascal Guilcher <guilcher.p@sfeir.com>
2013-12-12 17:04:48 +0100

    konami done

--- app/scripts/directives/ngKonami.js
+++ app/scripts/directives/ngKonami.js
@@ -1,10 +1,11 @@
 'use strict';
 
-angular.module('pguKonamiApp').directive('ngKonami', function () {
+angular.module('pguKonamiApp').directive('ngKonami', function ($timeout) {
 
     var KONAMI = String.fromCharCode(38, 38, 40, 40, 37, 39, 37, 39, 66, 65);
 
     var isAnimationOn = false;
+    var counter = 0;
 
     function startGoC() {
         if (isAnimationOn) {
@@ -13,18 +14,38 @@ angular.module('pguKonamiApp').directive('ngKonami', function () {
 
         isAnimationOn = true;
 
-        $('#goc').animate({marginTop: '2em'}, 5000);
-        $('#goc_container').fadeIn(2500).fadeOut(2500, function () {
-            $('#goc').css('margintop', '0.2em');
-        });
+        var modal = $('#k_modal');
+        var goc = $('#goc');
+        var meme = $('#meme');
 
-        isAnimationOn = false;
+        counter++;
+        meme.attr('src', 'images/' + (counter % 2 === 0 ? 'success_kid' : 'gow_bow') + '_300.gif');
+
+        modal.modal('show');
+
+        modal.addClass('black_bg');
+        goc.addClass('anim_goc');
+        meme.addClass('anim_meme');
+
+        $timeout(function () {
+            modal.fadeOut(2500, function() {
+                modal.modal('hide');
+
+                modal.removeClass('black_bg');
+                goc.removeClass('anim_goc');
+                meme.removeClass('anim_meme');
+
+                isAnimationOn = false;
+            });
+
+        }, 5000);
+
     }
 
     return {
         restrict: 'E',
         templateUrl: 'views/directives/ngKonami.html',
-        link: function (scope, element, attrs, ctrl, transcludeFn) {
+        link: function () {
 
             var codeBuffer = '';
 
